8a9cc974716463c3286a0e5d1a6a8a44c96baff1
Mhing <natthap411@gmail.com>
2023-12-16 22:37:45 +0700

    - install axios and tanstack query - test commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const API = process.env.BACKEND_PROXY_URL;
+const nextConfig = {
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path",
+				destination: `${API}/:path*`,
+			},
+		];
+	},
+};
+
+module.exports = nextConfig;
