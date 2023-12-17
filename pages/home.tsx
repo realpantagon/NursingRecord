0cde2adc17c90f249b9037f1c4b17cb955b6c142
Pantagon <pichayoothunchainao@gmail.com>
2023-12-17 17:38:37 +0700

    edit background color

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/component/navbar";
 
 export default function search() {
   return (
-    <div className={styles.background}>
+    <div className="bg-gradient-to-b from-teal-300 to-indigo-300  h-screen">
       <Navbar />
         <h1 className={styles.font}>ผู้ป่วยที่ต้องดูแล</h1>
         <div className={styles.contentcontainer}>
