6a8cfe7fc48f776c26f5b94947aba4734325df1a
Pantagon <pichayoothunchainao@gmail.com>
2023-12-17 17:37:10 +0700

    create new ndx page

--- pages/Form/ndx.tsx
+++ pages/Form/ndx.tsx
@@ -1,9 +1,10 @@
 import style from '../../styles/ndx.module.css';
+import Navbar from './../../component/navbar';
 
 export default function ndxform(){
     return(
-        <div className="">
-            Hi
+        <div className="bg-gradient-to-b from-teal-400 to-indigo-400  h-screen">
+            <Navbar/>
         </div>
     )
 }
