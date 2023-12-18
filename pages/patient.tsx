1b9a469f7a00470c06e1edb87a30bb395bc264a9
Pantagon <pichayoothunchainao@gmail.com>
2023-12-18 21:43:12 +0700

    fix color

--- pages/patient.tsx
+++ pages/patient.tsx
@@ -5,7 +5,7 @@ import { Button } from "primereact/button";
 import { Pagination } from "@mantine/core";
 import Link from "next/link";
 
-import ndxform from "./Form/ndx";
+import ndxform from "./Form/form";
 
 export default function Patient() {
   return (
@@ -13,116 +13,52 @@ export default function Patient() {
       <Navbar />
       <h1 className={styles.font}>ข้อมูลผู้ป่วย</h1>
       <div className={styles.contentcontainer}>
-        <div className="bg-gray-200 mx-8 my-8 rounded-md  px-8 py-4 grid md:grid-cols-2  lg:grid-cols-5 items-center">
+        <div className="bg-white mx-8 my-8 rounded-md  px-8 py-4 grid md:grid-cols-2  lg:grid-cols-5 items-center">
           <div className="my-2">ชื่อ:</div>
           <div className="my-2">วันเกิด:</div>
           <div className="my-2">Bed Number:</div>
           <div className="my-2">Admit Number:</div>
           <Button className="flex items-center justify-center md:col-span-2 lg:col-span-1">
-          <Link href="../Form/ndx" className="no-underline text-white w-full">
+          <Link href="../Form/form" className="no-underline text-white w-full">
             เพิ่มการบันทึก
           </Link>
           </Button>
         </div>
         <div className=" h-full gap-2  mx-4 mb-8 rounded-md px-8 py-4 grid lg:grid-cols-3 md:grid-cols-2 items-center ">
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
+          <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+            <Link href="../Form/form" className="no-underline text-black">
               <div className="mx-8 py-4">
                 <div className="">เวลาบันทึก:</div>
                 <div className="">NDX:</div>
               </div>
             </Link>
           </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
+          <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+            <Link href="../Form/form" className="no-underline text-black">
               <div className="mx-8 py-4">
                 <div className="">เวลาบันทึก:</div>
                 <div className="">NDX:</div>
               </div>
             </Link>
           </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
+          <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+            <Link href="../Form/form" className="no-underline text-black">
               <div className="mx-8 py-4">
                 <div className="">เวลาบันทึก:</div>
                 <div className="">NDX:</div>
               </div>
             </Link>
           </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
+          <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+            <Link href="../Form/form" className="no-underline text-black">
               <div className="mx-8 py-4">
                 <div className="">เวลาบันทึก:</div>
                 <div className="">NDX:</div>
               </div>
             </Link>
           </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
-              <div className="mx-8 py-4">
-                <div className="">เวลาบันทึก:</div>
-                <div className="">NDX:</div>
-              </div>
-            </Link>
-          </div>
-          <div className="rounded-md bg-gray-200 h-full hover:bg-gray-300 hover:shadow-lg">
-            <Link href="../Form/ndx" className="no-underline text-black">
+          <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+            <Link href="../Form/form" className="no-underline text-black">
               <div className="mx-8 py-4">
                 <div className="">เวลาบันทึก:</div>
                 <div className="">NDX:</div>
