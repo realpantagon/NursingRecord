91d2c477f7459065f487b945027b274f1cac3db2
Pantagon <pichayoothunchainao@gmail.com>
2023-12-19 23:46:24 +0700

    mock data in home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/home.module.css";
 import Navbar from "@/component/navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,173 @@ export default function Home() {
       <Navbar />
         <h1 className="text-center text-3xl ">ผู้ป่วยที่ต้องดูแล</h1>
         <div className={styles.contentcontainer}>
-        
+        <div className=" h-full gap-2 rounded-md px-4 py-4 grid lg:grid-cols-3 md:grid-cols-2 items-center ">
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                    <div className="">ชื่อ:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+            </div>
         </div>
     </div>
   );
