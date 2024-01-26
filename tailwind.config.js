95c87319897198ecaeb2cc5ab6e5e39fafd7bd9c
realpantagon <pichayoothunchainao@gmail.com>
2024-01-26 22:59:10 +0700

    install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+],
   corePlugins: {
     preflight: false,
   },
