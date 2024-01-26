3a546c3ba96370a762e3857a610f5fee062d9f43
realpantagon <pichayoothunchainao@gmail.com>
2024-01-26 22:41:35 +0700

    Apply new Appbar, Update design V2.2

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import styles from "../styles/home.module.css";
-
-import Navbar from "@/component/navbar";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
+import { Dropdown } from "primereact/dropdown";
 import Link from "next/link";
-import ndxform from "./Form/form";
+import Appbar from "@/component/Appbar";
 
 export default function Search() {
   const [selectedCity, setSelectedCity] = useState(null);
@@ -17,11 +15,11 @@ export default function Search() {
   ];
 
   return (
-    <div className="bg-gradient-to-b from-teal-300 to-indigo-300  h-screen">
-      <Navbar />
+     <div className="bg-stone-100  h-screen">
+      <Appbar/>
       <h1 className="text-center text-3xl">ค้นหาผู้ป่วย</h1>
-      <div className={styles.barcontainer}>
-        <div className={styles.butgroup}>
+      <div >
+        <div>
           <div className="p-inputgroup flex-1 max-w-md sm:max-w-xl md:max-w-2xl mx-auto">
             <Dropdown
               value={selectedCity}
@@ -33,16 +31,28 @@ export default function Search() {
             ></Dropdown>
             <InputText placeholder="Keyword" className={styles.input} />
             <Link href="patient">
-              <Button label="ค้นหา" className="" />
+              <Button label="ค้นหา" />
             </Link>
           </div>
-          <div className={styles.contentcontainer}>
+          <div >
             <div className=" h-full gap-2 rounded-md px-4 py-4 grid lg:grid-cols-3 md:grid-cols-2 items-center ">
               <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
                 <Link href="../patient" className="no-underline text-black">
                   <div className="mx-8 py-4">
                   <div className="">ชื่อ:</div>
                     <div className="">ผู้ป่วย:</div>
+                    <div className="">เตียง:</div>
+                    <div className="">บันทึกล่าสุด:</div>
+                    <div className="">Admit Number:</div>
+                    <div className="">วันเกิด:</div>
+                  </div>
+                </Link>
+              </div>
+              <div className="rounded-md bg-white h-full hover:bg-gray-300 hover:shadow-lg">
+                <Link href="../patient" className="no-underline text-black">
+                  <div className="mx-8 py-4">
+                  <div className="">ชื่อ:</div>
+                    <div className="">ค้นหาผู้ป่วย:</div>
                     <div className="">เตียง:</div>
                     <div className="">บันทึกล่าสุด:</div>
                     <div className="">Admit Number:</div>
