810eca40e1b950c131c269d4ad249a2bd2f6d954
realpantagon <pichayoothunchainao@gmail.com>
2024-01-27 20:37:05 +0700

    Fix logout on Navbar

--- component/Appbar.tsx
+++ component/Appbar.tsx
@@ -9,12 +9,12 @@ export default function Appbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // const { signOut } = useAuth();
+  const { signOut } = useAuth();
 
   return (
     <nav className=" border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="http://localhost:3000/" className="flex items-center space-x-3 rtl:space-x-reverse no-underline text-black">
+        <a href="/home" className="flex items-center space-x-3 rtl:space-x-reverse no-underline text-black">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" >Nursing Record</span>
         </a>
@@ -27,7 +27,7 @@ export default function Appbar() {
         >
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
           </svg>
         </button>
         <div className={`top-10 z-20 w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default" >
@@ -45,8 +45,7 @@ export default function Appbar() {
             <a href="/test" className="block py-2 px-3 text-black font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent no-underline">Test</a>
           </li>
           <li>
-            <div className="block py-2 px-3 text-black font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent no-underline">ออกจากระบบ</div>
-            {/* <div onClick={signOut} className="block py-2 px-3 text-black font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent no-underline">ออกจากระบบ</div> */}
+            <div onClick={signOut} className="block py-2 px-3 text-black font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent no-underline">ออกจากระบบ</div>
           </li>
         </ul>
       </div>
