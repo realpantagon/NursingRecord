db05d0bb18ccf5aed6ce68b2a9b8a771a12f054e
Pantagon <pichayoothunchainao@gmail.com>
2024-01-30 22:57:02 +0700

    position fix button amd form

--- pages/Form/FocusProblem/FocusProblemForm.tsx
+++ pages/Form/FocusProblem/FocusProblemForm.tsx
@@ -63,7 +63,7 @@ export default function FocusProblemForm() {
           <div className="my-2">Admit Number:</div>
           <div className="my-2">เวลาบันทึก:</div>
         </div>
-        <div className="flex flex-col justify-center mx-4 bg-white rounded-2xl">
+        <div className="flex flex-col justify-center mx-4 xl:mx-80 bg-white rounded-2xl">
           <div className="my-4 xl:mx-8 px-4 py-8 bg-teal-100 flex flex-col rounded-xl">
             <label className="mb-4 text-xl font-bold">ข้อมูลสนับสนุน</label>
             <textarea
@@ -97,7 +97,7 @@ export default function FocusProblemForm() {
             />
           </div>
         </div>
-        <div className="flex justify-between mx-4 xl:mx-16 my-4">
+        <div className="flex justify-between mx-4 xl:mx-80 my-4">
           <div
             className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 text-white px-12 py-2 rounded-md"
             onClick={submitfocusproblem}
