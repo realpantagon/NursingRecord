ae67ca9a85c83b08ec9be78fa7dc6c0fbca284e0
Pantagon <pichayoothunchainao@gmail.com>
2024-02-15 01:14:02 +0700

    real login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import PageLogin from './PageLogin';
 import Loginpage from './loginpage';
 
 export default function Home() {
@@ -7,7 +6,6 @@ export default function Home() {
     return (
         <div>
             <Loginpage></Loginpage>
-            {/* <PageLogin/> */}
         </div>
     );
 }
