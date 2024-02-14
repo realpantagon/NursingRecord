a279df409ff41fc58cd1533e47d64befbef82d30
Pantagon <pichayoothunchainao@gmail.com>
2024-02-15 00:51:07 +0700

    adjust sidebar

--- pages/Form/form.tsx
+++ pages/Form/form.tsx
@@ -1,45 +1,77 @@
+import { useState } from "react";
 import { TabView, TabPanel } from "primereact/tabview";
 import NDX from "./ndx";
 import Support from "./support";
 import Activities from "./Activities";
 import Evaluate from "./Evaluate";
-import { Button } from 'primereact/button';
+import { Button } from "primereact/button";
 import Link from "next/link";
 import Appbar from "@/component/Appbar";
 import PatientData from "@/component/PatientData/PatientData";
-
-
-interface ndx {
-  name: string;
-  key: Number;
-}
+import PatientHistory from "@/component/PatientData/PatientHistory";
+import { Sidebar } from "primereact/sidebar";
 
 export default function Form() {
-
+  const [visible, setVisible] = useState<boolean>(false);
 
   return (
-    <div className="bg-stone-100 w-full">
+    <div className="bg-stone-100 min-h-screen w-screen">
+      <Appbar />
+      <div className="card flex justify-content-center mx-8">
+        <Sidebar
+          visible={visible}
+          onHide={() => setVisible(false)}
+          className="w-full lg:w-3/12 md:w-20rem lg:w-30rem"
+        >
+          <Link
+            href="../Form/FocusProblem/FocusProblemForm"
+            className="no-underline text-white w-full "
+          >
+            <div className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-4 w-full shadow-sm">
+              เพิ่ม Focus Problem
+            </div>
+          </Link>
+            <Link
+              href="../Form/form"
+              className="no-underline text-white w-full "
+            >
+              <div className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-2 w-full shadow-sm">
+                เพิ่มแบบบันทึกทางการพยาบาล
+              </div>
+            </Link>
+            <hr />
+            <div className="text-sm ml-4">การบันทึกล่าสุด</div>
+            <PatientHistory />
+          <hr />
+          <PatientHistory />
+          <PatientHistory />
+          <PatientHistory />
+        </Sidebar>
+        <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
+      </div>
+
       <h1 className="text-center text-3xl m-0">กรอกข้อมูล</h1>
-      <div className="flex flex-col sm:mx-0 md:mx-8 pb-96">
-        <PatientData/>
-        <div className="rounded-lg pt-4 bg-white">
-        <TabView >
-          <TabPanel header="ข้อวินิจฉัย" className="mx-auto text-sm">
-            <NDX/>
-          </TabPanel>
-          <TabPanel header="ข้อมูลสนับสนุน" className="mx-auto text-sm">
-            <Support/>
-          </TabPanel>
-          <TabPanel header="กิจกรรมพยาบาล" className="mx-auto text-sm">
-            <Activities/>
-          </TabPanel>
-          <TabPanel header="การประเมินผล" className="mx-auto text-sm">
+
+      <div className="flex flex-col w-full">
+        <PatientData />
+        <div className="rounded-lg pt-4 bg-white mx-4 lg:mx-52">
+          <TabView>
+            <TabPanel header="ข้อวินิจฉย" className="mx-auto text-sm">
+              <NDX />
+            </TabPanel>
+            <TabPanel header="ข้อมูลสนับสนุน" className="mx-auto text-sm">
+              <Support />
+            </TabPanel>
+            <TabPanel header="กิจกรรมพยาบาล" className="mx-auto text-sm">
+              <Activities />
+            </TabPanel>
+            <TabPanel header="การประเมินผล" className="mx-auto text-sm">
             <Evaluate/>
             <div className="flex flex-row-reverse my-12">
             <Button className="flex w-full md:w-2/12 mx-8 " label="Submit" icon="pi pi-check" iconPos="right" />
             </div>
           </TabPanel>
-        </TabView>
+          </TabView>
         </div>
       </div>
     </div>
