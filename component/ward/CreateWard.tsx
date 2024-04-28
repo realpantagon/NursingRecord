f634a3b52e16c40aa2018f06a4161a684373bedd
realpantagon <pichayoothunchainao@gmail.com>
2024-04-28 19:05:59 +0700

    change to select ward page

---
+++ component/ward/CreateWard.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { ColorPicker, ColorPickerChangeEvent, ColorPickerRGBType, ColorPickerHSBType } from "primereact/colorpicker";
+import { Button } from "primereact/button";
+
+interface CreateWardProps {
+  visible: boolean;
+  onClose: () => void;
+}
+
+const CreateWard: React.FC<CreateWardProps> = ({ visible, onClose }) => {
+  const [color, setColor] = useState<string>("#000000");
+  const [wardName, setWardName] = useState<string>("");
+
+  useEffect(() => {
+    if (!visible) {
+      setColor("#000000");
+      setWardName("");
+    }
+  }, [visible]);
+
+  const handleColorChange = (e: ColorPickerChangeEvent) => {
+    const newColor = e.value as string | ColorPickerRGBType | ColorPickerHSBType;
+    if (typeof newColor === "string") {
+      setColor(newColor);
+    } else {
+      setColor("#FFFFFF");
+    }
+  };
+
+  const handleConfirm = () => {
+    console.log("Ward Name:", wardName);
+    console.log("Color Code:", color);
+    // Add any additional logic or API calls here
+
+    // Close the dialog
+    onClose();
+  };
+
+  return (
+    <Dialog header="เพิ่มกลุ่มโรค" visible={visible} style={{ width: "80vw" }} onHide={onClose} className="">
+      <div className="grid grid-cols-1 gap-4">
+        <p>ระบุชื่อกลุ่มโรคและเลือกสี</p>
+        <div className="flex">
+          <InputText placeholder="ชื่อกลุ่มโรค" value={wardName} onChange={(e) => setWardName(e.target.value)} />
+          <div className="ml-4 mt-2">
+            <ColorPicker value={color} onChange={handleColorChange} />
+          </div>
+        </div>
+        <Button label="ตกลง" onClick={handleConfirm} />
+      </div>
+    </Dialog>
+  );
+};
+
+export default CreateWard;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,21 +1,50 @@
-import React from "react";
-import Link from "next/link";
+import React,{useState} from "react";
 import Appbar from "@/component/Appbar";
-import PatientCard from "@/component/PatientData/PatientCard";
+import "primeicons/primeicons.css";
+import CreateWard from "@/component/ward/CreateWard";
+import WardCard from "@/component/ward/WardCard";
 
 export default function Home() {
+  const [isCreateWardDialogOpen, setCreateWardDialogOpen] = useState(false);
+
+  const openCreateWardDialog = () => {
+    setCreateWardDialogOpen(true);
+  };
+
+  const closeCreateWardDialog = () => {
+    setCreateWardDialogOpen(false);
+  };
+
   return (
     <div className="bg-stone-100  min-h-screen">
       <Appbar />
-      <h1 className="text-center text-3xl ">ผู้ป่วยที่ต้องดูแล</h1>
+      <h1 className="text-center text-3xl ">เลือกกลุ่มโรค</h1>
       <div className="xl:mx-52">
-        <div className=" h-full gap-2 rounded-md px-4 py-4 grid lg:grid-cols-3 md:grid-cols-2 items-center ">
-          {/* <PatientCard/>
-          <PatientCard/>
-          <PatientCard/>
-          <PatientCard/> */}
+        <div className=" h-full gap-2 rounded-md px-4 py-4 grid lg:grid-cols-4 md:grid-cols-2 items-center ">
+          <WardCard/>
+          <WardCard/>
+          <WardCard/>
+          <WardCard/>
+          <WardCard/>
+          <WardCard/>
+          <WardCard/>
+          <div
+            className="rounded-md bg-white h-56 hover:bg-gray-300 hover:shadow-lg shadow-md flex items-center justify-center"
+            onClick={openCreateWardDialog}
+          >
+            <i
+              className="pi pi-plus-circle"
+              style={{ fontSize: "2rem", color: "black" }}
+            />
+            <div />
+            <h2> เพิ่มกลุ่มโรค</h2>
+          </div>
         </div>
       </div>
+      <CreateWard
+        visible={isCreateWardDialogOpen}
+        onClose={closeCreateWardDialog}
+      />
     </div>
   );
 }
