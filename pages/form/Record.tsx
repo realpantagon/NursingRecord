2af6916df5a0abfc133dd554c74ec52974e77271
realpantagon <pichayoothunchainao@gmail.com>
2024-04-30 10:25:04 +0700

    new Record page Design

--- pages/form/Record.tsx
+++ pages/form/Record.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Appbar from "@/component/Appbar";
 import PatientData from "@/component/patientData/PatientData";
 import Focusproblemcard from "@/component/patientData/Focusproblemcard";
 import Focuschartcard from "@/component/patientData/Focuschartcard";
+import { TabView, TabPanel } from "primereact/tabview";
 
 export default function Form() {
   return (
@@ -14,27 +15,25 @@ export default function Form() {
           <div className="2">
             <PatientData />
 
-              <Link
-                href="../form/focusProblem/FocusProblemForm"
-                className="no-underline text-white w-full "
-              >
-                <div className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-4 w-full shadow-sm">
-                  เพิ่ม Focus Problem
-                </div>
-              </Link>
-              <Link
-                href="../Form/Form"
-                className="no-underline text-white w-full "
-              >
-                <div className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-2 w-full shadow-sm">
-                  เพิ่มแบบบันทึกทางการพยาบาล
-                </div>
-              </Link>
-
-            
+            <Link
+              href="../form/focusProblem/FocusProblemForm"
+              className="no-underline text-white w-full "
+            >
+              <div className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-4 w-full shadow-sm">
+                เพิ่ม Focus Problem
+              </div>
+            </Link>
+            <Link
+              href="../Form/Form"
+              className="no-underline text-white w-full "
+            >
+              <div className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-md px-5 py-4 text-center me-2 mb-2 w-full shadow-sm">
+                เพิ่มแบบบันทึกทางการพยาบาล
+              </div>
+            </Link>
           </div>
         </div>
-        <div className="lg:w-5/12 w-11/12 mx-2 bg-sky-100 rounded-md">
+        {/* <div className="lg:w-5/12 w-11/12 mx-2 bg-sky-100 rounded-md">
           <div className="mx-4">
             <h1>Focus Chart</h1>
             <hr />
@@ -57,6 +56,51 @@ export default function Form() {
             <Focusproblemcard />
             <Focusproblemcard />
           </div>
+        </div> */}
+        <div className="lg:w-10/12 w-11/12 mx-2 bg-sky-100 rounded-md">
+          <TabView>
+            <TabPanel header="FocusChart">
+              <div className="m-2 grid md:grid-cols-4 gap-2 grid-cols-1">
+                {/* <h1 className="col-span-full">Focus Chart</h1>
+                <hr className="col-span-full" />
+                <div className="text-sm ml-4">การบันทึกล่าสุด</div> */}
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+                <Focuschartcard />
+              </div>
+            </TabPanel>
+            <TabPanel header="FocusProblem">
+              <div className="m-2 grid md:grid-cols-4 gap-2 grid-cols-1">
+                {/* <h1 className="col-span-full">Focus Problem</h1> */}
+                {/* <hr className="col-span-full" /> */}
+                {/* <div className="text-sm ml-4">การบันทึกล่าสุด</div> */}
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+                <Focusproblemcard />
+              </div>
+            </TabPanel>
+            {/* <TabPanel header="Filter">
+              <p className="m-0"></p>
+            </TabPanel> */}
+          </TabView>
         </div>
       </div>
     </div>
