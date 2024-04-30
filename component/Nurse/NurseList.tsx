7a111628a4d4227aa4d2c526042e197677e16a49
realpantagon <pichayoothunchainao@gmail.com>
2024-04-30 17:34:23 +0700

    Edit Nurse Roles

---
+++ component/Nurse/NurseList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NurseList() {
+  return (
+    <div></div>
+  )
+}
+
+export default NurseList
--- pages/admin/EditRoles.tsx
+++ pages/admin/EditRoles.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Appbar from "@/component/Appbar";
+import NurseList from "@/component/Nurse/NurseList";
 
 function EditRoles() {
   return (
     <div className="bg-stone-100 min-h-screen">
       <Appbar />
       <h1 className="text-center text-3xl">แก้ไขสิทธิ์พยาบาล</h1>
+      <div className=" flex flex-col items-center">
+      <div className=" bg-white w-full md:w-8/12 p-4 rounded-md">
+        <NurseList/>
+      </div>
+      </div>
+
+      
     </div>
   );
 }
